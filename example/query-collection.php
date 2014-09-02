23716c386fa5a5d859d00804cfd01698d3539182
Jeremy Mikola <jmikola@gmail.com>
2014-09-02 16:35:20 -0400

    Demo for querying a collection

---
+++ example/query-collection.php
@@ -0,0 +1,53 @@
+<?php
+
+use Jmikola\React\MongoDB\Connection;
+use Jmikola\React\MongoDB\ConnectionFactory;
+use Jmikola\React\MongoDB\Protocol\Query;
+use Jmikola\React\MongoDB\Protocol\Reply;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+$factory = new ConnectionFactory($loop);
+
+$connection = $factory->create('127.0.0.1', 27017)->then(
+    function (Connection $connection) {
+        $connection->on('close', function() {
+            printf("# connection closed\n");
+        });
+
+        $connection->on('error', function(Exception $e) {
+            printf("# connection error: %s\n", $e->getMessage());
+            printf("%s\n", $e->getTraceAsString());
+        });
+
+        $connection->on('message', function(Reply $reply) {
+            printf("# received reply of size: %d\n", $reply->getMessageLength());
+        });
+
+        $message = new Query('test.foo', (object) []);
+
+        $connection->send($message)->then(
+            function(Reply $reply) {
+                printf("# query executed successfully!\n");
+                foreach ($reply as $document) {
+                    printf("Document id: %s\n", $document['_id']);
+                }
+            },
+            function (Exception $e) {
+                printf("# query error: %s\n", $e->getMessage());
+                printf("%s\n", $e->getTraceAsString());
+            }
+        );
+
+        // TODO: Implement GetMore and iteration on full result set
+
+        $connection->end();
+    },
+    function (Exception $e) {
+        printf("# connection error: %s\n", $e->getMessage());
+        printf("%s\n", $e->getTraceAsString());
+    }
+);
+
+$loop->run();
