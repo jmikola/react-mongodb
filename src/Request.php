fb3fbc5097b2277d96cc905804f26da2abe73e59
Honza Machala <jan.machala@bileto.cz>
2016-04-13 14:01:42 +0200

    Doc comments for help IDE to auto-complete

--- src/Request.php
+++ src/Request.php
@@ -9,16 +9,26 @@ class Request extends Deferred
 {
     private $requestId;
 
+    /**
+     * Request constructor.
+     * @param int $requestId
+     */
     public function __construct($requestId)
     {
         $this->requestId = $requestId;
     }
 
+    /**
+     * @return int
+     */
     public function getRequestId()
     {
         return $this->requestId;
     }
 
+    /**
+     * @param Reply $reply
+     */
     public function handleReply(Reply $reply)
     {
         // TODO: Check reply for error and call Deferred::reject()
